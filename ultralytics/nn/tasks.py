836e55e06e0d214b2b4c0529beecff91c2c63a7f
hr-02 <55356556+hr-02@users.noreply.github.com>
2025-05-08 02:55:12 +0530

    Update tasks.py
    Signed-off-by: hr-02 <55356556+hr-02@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1482,7 +1482,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             c2 = args[0]
             c1 = ch[f]
             args = [c1, c2, *args[1:]]
-        elif m in {CBAM}:
+        elif m is CBAM:
             c1, c2 = ch[f], args[0]
             if c2 != nc:
                 c2 = make_divisible(min(c2, max_channels) * width, 8)
